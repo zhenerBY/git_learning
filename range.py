fd14e11bac600aff5d8bdc6c1a6f9e9e1366db82
zhenerBY <zhener@tut.by>
2021-09-04 23:39:27 +0300

    update range

--- range.py
+++ range.py
@@ -1,7 +1,15 @@
-def rg(a: int, b: int, c: int = 1) -> list:
-    """Замена RANGE"""
+def rg(a: int, b: int = '@$^*fhYLu', c: int = '@$^*fhYLu') -> tuple:
+    """Замена RANGE (tuple)"""
+    if b == c and b == '@$^*fhYLu':
+        b = a
+        a = 0
+        c = 1
+    elif c == '@$^*fhYLu':
+        c = 1
     list1 = []
     n = int(a)
+    b = int(b)
+    c = int(c)
     if c == 0:
         return list1
     elif c > 0:
@@ -12,7 +20,7 @@ def rg(a: int, b: int, c: int = 1) -> list:
         while n > b:
             list1.append(n)
             n += c
-    return list1
+    return tuple(list1)
 
 
-print(rg(-10, -22, 1))
+print(rg(-10, 22, 1))
